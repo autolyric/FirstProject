797d880c73e790db39e6c74999a12551ec25eed5
autolyric <464741080@sohu.com>
2021-07-12 09:25:34 +0800

    删除项目内多余文件

--- src/projects/KE06/Test/System.h
+++ src/projects/KE06/Test/System.h
@@ -15,34 +15,6 @@
 #include "mscan.h"
 
 
-#include "input.h"
-#include "LIN_Slave.h"
-#include "MotorCtrl.h"
-
-typedef struct
-{
-	uint8_t sleep;
-	uint8_t power;
-	uint8_t onoff;
-}MoudleStat_T;
-
-
-#define MOUDLE_SLEEP   0x01
-#define MOUDLE_AWAKE   0x01
-
-
-#define  MOUDLE_POWER_OVERLOW  0x00
-#define  MOUDLE_POWER_LOW      0x01
-#define  MOUDLE_POWER_NORMAL   0x02
-#define  MOUDLE_POWER_HIGH     0x03
-#define  MOUDLE_POWER_OVERHIGH 0x04
-
-#define  MOUDLE_ON     0x01
-#define  MOUDLE_OFF    0x00
-
-
-#define SPI_BIT_RATE    	1000000     /* ~1Mbps */
-
 #define SIM_SCGC_VALUE      0x00003000L  
 
 
